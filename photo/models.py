8817511e94f4740e6e4d8aa7d12b59464af69a47
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-09-12 22:38:52 +0300

    add the model fields to model profile

--- photo/models.py
+++ photo/models.py
@@ -1,6 +1,10 @@
 from django.db import models
-
+from tinymce.models import HTMLField
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Profile(models.Model):
-    
+    profilePic = models.ImageField(upload_to='profileimages/', null=True, blank=True, default='profileimages/test.png')
+    fullName= models.CharField(max_length=255, null=True)
+    username = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = HTMLField(null=True, blank=True)
