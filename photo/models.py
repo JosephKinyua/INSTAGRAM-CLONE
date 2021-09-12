09d506671bdf30d74525eb8144fa44ca2eae6f38
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-09-12 22:53:11 +0300

    add a modle like

--- photo/models.py
+++ photo/models.py
@@ -24,3 +24,5 @@ class Comments(models.Model):
 class Follow(models.Model):
     follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name='follower')
     following = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following')
+
+class Likes(models.Model):
