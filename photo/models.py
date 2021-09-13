4b6424833296724c67cea07e72e1f3085178501d
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-09-13 08:40:36 +0300

    add receivers for profile creation and save

--- photo/models.py
+++ photo/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from tinymce.models import HTMLField
 from django.contrib.auth.models import User
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
 
 class Profile(models.Model):
@@ -8,6 +11,15 @@ class Profile(models.Model):
     fullName= models.CharField(max_length=255, null=True)
     username = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = HTMLField(null=True, blank=True)
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(username=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+         instance.profile.save()
 
     def __str__(self):
         return self.username
