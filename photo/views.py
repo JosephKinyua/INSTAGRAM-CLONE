0f2abc0d3b4438c1835e348314ee54f062c749a1
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-09-13 09:04:21 +0300

    create a home view

--- photo/views.py
+++ photo/views.py
@@ -1,8 +1,18 @@
 from django.http  import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Post
 
 # Create your views here.
 @login_required(login_url='/accounts/login/')
 def home(request):
-    return render(request, 'home.html')
+    if request.method == 'POST':
+        commentForm = CommentForm(request.POST)
+        likeform = LikeForm(request.POST)
+        if commentForm.is_valid():
+            pic_id = int(request.POST.get('imageid'))
+            pic = Post.objects.get(id=pic_id)
+            com = commentForm.save(commit=False)
+            com.user = request.user
+            com.pic = pic
+            com.save()
