125c475077ec3658a915889bf4213187a41c19bd
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-09-15 05:51:19 +0300

    add a profile

--- photo/urls.py
+++ photo/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     path('',views.home,name = 'home'),
+    path('account/profile', views.profile, name='uprofile'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- photo/views.py
+++ photo/views.py
@@ -2,7 +2,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Post, Profile,Comments
-from .forms import CommentForm, LikeForm
+from .forms import UserForm, ProfileForm,CommentForm, LikeForm
 from django.contrib import messages
 from django.http import Http404, HttpResponseRedirect, JsonResponse
 from django.contrib.auth.models import User
@@ -29,3 +29,19 @@ def home(request):
   likeform = LikeForm()
   allcomments = Comments.objects.all()
   return render(request, 'home.html', {'title': title, 'allpics':allpics, 'commentForm':commentForm, 'allcomments':allcomments, 'likeform':likeform})
+@login_required(login_url='accounts/login')
+def profile(request):
+  if request.method == 'POST':
+    user_form = UserForm(request.POST, instance=request.user)
+    profile_form = ProfileForm(request.POST, request.FILES, instance=request.user.profile)
+    if user_form.is_valid() and profile_form.is_valid():
+      user_form.save()
+      profile_form.save()
+      messages.success(request,('Your profile was successfully updated!'))
+    return redirect('uprofile')
+
+  title = 'Profile'
+  user_form = UserForm(instance=request.user)
+  profile_form = ProfileForm(instance=request.user)
+  user_pics = Post.user_pictures(request.user.username)
+  return render(request, 'profile/home.html', {'title':title, "user":request.user, "user_form":user_form, 'profile_form':profile_form, 'user_pics':user_pics})
